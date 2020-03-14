5101f4c965845ea855faf5f3be95931e4e5164a9
健太 <kenta@mba.local>
2020-03-14 23:07:48 +0900

    create_second_page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,8 @@
 class QuestionsController < ApplicationController
     def show
-        @question = Question.find(1)
+        @question = Question.find(params.require(:id))
         @choices = @question.choices
     end
 end
+
+
---
+++ app/javascript/question.js
@@ -0,0 +1,6 @@
+
+$(document).on('turbolinks:load', function(){
+    $(".qustion_num").click(function(){
+        $(".qustion_num").css("color","Red");
+    });
+});
