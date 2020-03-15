0b83eb681559f5be8b3899eeb760db3c2d034fbe
健太 <kenta@mba.local>
2020-03-15 12:04:00 +0900

    ゴリ押し感はすごいけど一旦完成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,18 @@
 class QuestionsController < ApplicationController
     def show
-        @question = Question.find(params.require(:id))
+        if params.require(:id) == "1"
+            @question = Question.find(1)
+        elsif params.require(:id) == "2"
+            @question = Question.find(16)
+        elsif params.require(:id) == "3"
+            @question = Question.find(17)
+        elsif params.require(:id) == "4"
+            @question = Question.find(18)
+        elsif params.require(:id) == "5"
+            @question = Question.find(19)
+        else
+
+        end
         @choices = @question.choices
     end
 end
