657cfab44dc10bb256dd60ad9a329b11eb4b5423
健太 <kenta@mba.local>
2020-03-20 15:29:29 +0900

    questionの番号を正しいものに直した

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,11 @@
 class QuestionsController < ApplicationController
     def show
         if params.require(:id) == "1"
+            @question_num = 1
             @question = Question.find(1)
             @choices = @question.choices
         elsif params.require(:id).to_i >= 2 && params.require(:id).to_i <=6
+            @question_num = 2
             @question = Question.find(params.require(:id).to_i+14)
             @choices = @question.choices
         elsif params.require(:id).to_i >= 7
