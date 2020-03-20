110156331e515ecf99a1b4c06f0ba0d7d5c9284c
健太 <kenta@mba.local>
2020-03-20 10:21:35 +0900

    とりあえずブランド名が表示されるようになった

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,18 +2,23 @@ class QuestionsController < ApplicationController
     def show
         if params.require(:id) == "1"
             @question = Question.find(1)
+            @choices = @question.choices
         elsif params.require(:id) == "2"
             @question = Question.find(16)
+            @choices = @question.choices
         elsif params.require(:id) == "3"
             @question = Question.find(17)
+            @choices = @question.choices
         elsif params.require(:id) == "4"
             @question = Question.find(18)
+            @choices = @question.choices
         elsif params.require(:id) == "5"
             @question = Question.find(19)
-        else
-
+            @choices = @question.choices
+        elsif params.require(:id).to_i-1 >= 6
+            @brands = Choice.find(params.require(:id).to_i-1).brands
+            render("brand/show")
         end
-        @choices = @question.choices
     end
 end
 
--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,6 @@
 class Brand < ApplicationRecord
+    belongs_to :choice
+    def show
+        
+    end
 end
