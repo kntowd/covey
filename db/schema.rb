c2b75598e2b67a3ddc86a7fdf5d1077658a34009
健太 <kenta@mba.local>
2020-03-22 16:19:02 +0900

    add image column and add image to crepscule page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_013533) do
+ActiveRecord::Schema.define(version: 2020_03_22_065012) do
 
   create_table "brands", force: :cascade do |t|
     t.string "text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_03_15_013533) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "image_name"
   end
 
   create_table "choices", force: :cascade do |t|
